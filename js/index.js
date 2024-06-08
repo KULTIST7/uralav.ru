f4b39d22cdb68f913bf4ea119ca2ef50dca5e12d
KULTIST7 <kultist77@gmail.com>
2024-06-08 13:13:38 +0500

    inner pages

--- js/index.js
+++ js/index.js
@@ -107,5 +107,15 @@ $(document).ready(function () {
         }
     });
 
+    $('input[type=tel]').inputmask({
+        mask: '+7 (*{1}99) 999-99-99',
+        placeholder: "+7 (___) ___-__-__",
+        definitions: {
+            '*': {
+                validator: "[0-6,9]"
+            }
+        }
+    });
+
     Fancybox.bind("[data-fancybox]");
 });
